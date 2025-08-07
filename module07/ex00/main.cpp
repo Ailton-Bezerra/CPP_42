7527e27f6a557148f7e3a27ba16773b6c3143cd2
Ailton <ailbezer@c2r6p2.42sp.org.br>
2025-08-07 18:52:45 -0300

    feat(mod07): modify tests

--- module07/ex00/main.cpp
+++ module07/ex00/main.cpp
@@ -6,11 +6,30 @@
 /*   By: ailbezer <ailbezer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/07 13:31:15 by ailbezer          #+#    #+#             */
-/*   Updated: 2025/08/07 13:31:18 by ailbezer         ###   ########.fr       */
+/*   Updated: 2025/08/07 13:44:27 by ailbezer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
+
+// class Awesome
+// {
+// 	public:
+// 		Awesome(void) : _n(0) {}
+// 		Awesome( int n ) : _n( n ) {}
+// 		Awesome & operator= (Awesome & a) { _n = a._n; return *this; }
+// 		bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
+// 		bool operator!=( Awesome const & rhs ) const{ return (this->_n != rhs._n); }
+// 		bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); }
+// 		bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); }
+// 		bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
+// 		bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); }
+// 		int get_n() const { return _n; }
+		
+// 	private:
+// 		int _n;
+// };
+// std::ostream & operator<<(std::ostream & o, const Awesome &a) { o << a.get_n(); return o; }
 
 int main () {
 
@@ -18,17 +37,22 @@ int main () {
 	int b = 3;
 
 	::swap(a, b);
-	std::cout << "a = " << a << ", b= " << b << std::endl;
+	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min(a, b) = " <<::min(a, b) << std::endl;
 	std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
 
 	std::string c = "chaine1";
 	std::string d = "chaine2";
 
-        ::swap(c, d);
-        std::cout << "c = " << c << ", d= " << d << std::endl;
-        std::cout << "min(c, d) = " <<::min(c, d) << std::endl;
-        std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min(c, d) = " <<::min(c, d) << std::endl;
+	std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
 
+	// Awesome a(2), b(4);
+	// swap(a, b);
+	// std::cout << a << " " << b << std::endl;
+	// std::cout << max(a, b) << std::endl;
+	
 	return (0);
 }
--- module07/ex01/main.cpp
+++ module07/ex01/main.cpp
@@ -5,15 +5,35 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ailbezer <ailbezer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/07 13:31:50 by ailbezer          #+#    #+#             */
-/*   Updated: 2025/08/07 13:31:52 by ailbezer         ###   ########.fr       */
+/*   Created: 2025/08/07 18:35:14 by ailbezer          #+#    #+#             */
+/*   Updated: 2025/08/07 18:41:12 by ailbezer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
+// class Awesome
+// {
+// 	public:
+// 		Awesome( void ) : _n( 42 ) { return; }
+// 		int get( void ) const { return this->_n; }
+// 	private:
+// 		int _n;
+// };
+
+// std::ostream & operator<<( std::ostream & o, Awesome const & rhs ) { o << rhs.get(); return o; }
+
+// template< typename T >
+// void print( T const & x ) { std::cout << x << std::endl; return; }
+
 int main() {
 	int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 	::iter(array, 9, printElement<int>);
+	
+	// int tab[] = { 0, 1, 2, 3, 4 };
+	// Awesome tab2[5];
+	// ::iter( tab, 5, print<int> );
+	// ::iter( tab2, 5, print<Awesome> );
+	
 	return (0);
 }

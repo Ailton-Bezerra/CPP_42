05a595e29b28b074400f6a56c4fff1897d3a00cd
Ailton <ailbezer@c2r6p2.42sp.org.br>
2025-08-12 17:16:30 -0300

    refactor(mod08 ex02): improved main

--- module08/ex02/main.cpp
+++ module08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ailbezer <ailbezer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 16:00:24 by ailbezer          #+#    #+#             */
-/*   Updated: 2025/08/12 16:00:28 by ailbezer         ###   ########.fr       */
+/*   Updated: 2025/08/12 17:12:38 by ailbezer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ int main()
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
 	mstack.push(0);
 
 	MutantStack<int>::iterator it = mstack.begin();
@@ -38,6 +37,33 @@ int main()
 		++it;
 	}
 
-//	std::stack<int> s(mstack);
+	std::cout << std::endl;
+	std::cout << "================ More tests ================" << std::endl;
+	
+	std::cout << "Atual stack:" << std::endl;
+	it = mstack.begin();
+	while (it != ite) {
+		std::cout << *it << std::endl;
+		++it;
+	}
+	
+	std::cout << std::endl;
+	std::cout << "Element at beginz\t(first):\t";
+	MutantStack<int>::iterator begin = mstack.begin();
+	std::cout << *begin << std::endl;
+
+	std::cout << "Element at ++rbegin\t(second-last):\t";
+	MutantStack<int>::reverse_iterator rbegin = mstack.rbegin();
+	++rbegin;
+	std::cout << *rbegin << std::endl;
+	
+	std::cout << "Element at end\t\t(last):\t\t";
+	MutantStack<int>::iterator end = mstack.end();
+	std::cout << *end << std::endl;
+	
+	std::cout << "Element at --rend\t(first):\t";
+	MutantStack<int>::reverse_iterator rend = mstack.rend();
+	std::cout << *(--rend) << std::endl;
+	
 	return (0);
 }
